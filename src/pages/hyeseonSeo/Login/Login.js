55c1f8d0e6506c65bc02773750930f896e140555
seonday <gptjs0613@gmail.com>
2022-12-02 20:34:16 +0900

    REFACTOR: live code review 이후 수정

--- src/pages/hyeseonSeo/Login/Login.js
+++ src/pages/hyeseonSeo/Login/Login.js
@@ -3,12 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({ id: '', password: '' });
   const navigate = useNavigate();
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
 
-  const isPassed = id.includes('@') && password.length >= 5;
-  const checkPass = isPassed ? false : true;
+  function checkUser(e) {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  }
+
+  const isPassed = userInfo.id.includes('@') && userInfo.password.length >= 5;
 
   return (
     <header className="Login">
@@ -16,18 +19,16 @@ const Login = () => {
         <h1>Westagram</h1>
         <form className="loginForm">
           <input
-            onChange={({ target }) => {
-              setId(target.value);
-            }}
+            name="id"
+            onChange={checkUser}
             className="loginId"
-            type="email"
+            type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             maxLength="50"
           />
           <input
-            onChange={({ target }) => {
-              setPassword(target.value);
-            }}
+            name="password"
+            onChange={checkUser}
             className="loginPw"
             type="password"
             placeholder="비밀번호"
@@ -35,10 +36,25 @@ const Login = () => {
           <span className="alertComment">로그인에 실패했습니다</span>
           <button
             className={isPassed ? 'loginBtnActive' : 'loginBtn'}
-            disabled={checkPass}
-            onClick={() => {
+            onClick={e => {
               navigate('/main-hs');
+              // e.preventDefault();
+              //   fetch('http://10.58.52.93:3000/user/signin', {
+              //     method: 'POST',
+              //     headers: {
+              //       'Content-Type': 'application/json;charset=utf-8',
+              //     },
+              //     body: JSON.stringify({
+              //       // name: '7조',
+              //       // profileImage: './images/hyeseonSeo/camping.jpg',
+              //       email: 'gptjs@gmail.com',
+              //       password: 'password',
+              //     }),
+              //   })
+              //     .then(res => res.json())
+              //     .then(data => console.log(data));
             }}
+            disabled={!isPassed}
           >
             로그인
           </button>
